039c941067caacd906a3797ebc31e1f76564a4c8
Marco VALENTINO <mvalentino@jurasix24.idiap.ch>
2023-07-03 13:18:39 +0200

    translational model

--- latent_reasoning/TranslationalLatentReasoning.py
+++ latent_reasoning/TranslationalLatentReasoning.py
@@ -1,5 +1,4 @@
 from transformers import AutoTokenizer, AutoModel
-#from sentence_transformers import SentenceTransformer, util
 import torch
 from torch import nn
 import numpy as np
@@ -8,25 +7,27 @@ import numpy as np
 class TransLatentReasoning(nn.Module):
     def __init__(self, model_name, n_operations, device):
         super(TransLatentReasoning, self).__init__()
-        # Load model from HuggingFace Hub
-        #'sentence-transformers/bert-base-nli-mean-tokens'
         self.device = device
+        # Load model from HuggingFace Hub
         self.encoder = AutoModel.from_pretrained(model_name).to(device)
         self.dim = 768
-       #self.encoder_transf = AutoModel.from_pretrained(model_name).to(device)
+        #self.encoder_transf = AutoModel.from_pretrained(model_name).to(device)
         self.linear = nn.Linear(self.dim*3, self.dim).to(device)
+        
         self.ov = nn.Embedding(n_operations, self.dim, padding_idx=0)
         self.ov.weight.data = (1e-3 * torch.randn((n_operations, self.dim), dtype=torch.float, device = device))
         self.Wo = torch.nn.Parameter(torch.tensor(np.random.uniform(-1,1, (n_operations, self.dim)), dtype=torch.float, requires_grad=True, device=self.device))
-        self.similarity_fct = nn.functional.cosine_similarity #util.cos_sim
-        self.loss_function = nn.MSELoss() #nn.BCEWithLogitsLoss() #nn.MSELoss()
+        
+        self.similarity_fct = nn.functional.cosine_similarity
+        self.loss_function = nn.MSELoss()
 
         
     def forward(self, equation1, equation2, target_equation, operation, labels):
+        # GET OPERATION EMBEDDINGS
         Wo = self.Wo[operation]
         ov = self.ov.weight[operation]
 
-        # Compute embeddings
+        # ENCODE EQUATIONS
         equation1 = {k: v.to(self.device) for k, v in equation1.items()}
         embeddings_eq1 = self.encoder(**equation1)
         embeddings_eq1 = self.mean_pooling(embeddings_eq1, equation1['attention_mask'])
@@ -46,8 +47,8 @@ class TransLatentReasoning(nn.Module):
         embeddings_output = embeddings_output * Wo
         embeddings_target = embeddings_target + ov
 
+        #COMPUTE LOSS
         scores = self.similarity_fct(embeddings_output, embeddings_target)
-
         labels = labels.to(self.device)
         loss = self.loss_function(scores, labels)
 
