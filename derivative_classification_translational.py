d7859f3a96d3b4290bdedc2df9f13c7a4fca7dbf
mValentino91 <marco@DESKTOP-8HJ11VO.localdomain>
2023-07-04 11:06:50 +0100

    updated translational training with contrast evaluation

--- derivative_classification_translational.py
+++ derivative_classification_translational.py
@@ -14,6 +14,7 @@ class Experiment:
 
     def __init__(self, learning_rate, model, epochs, batch_size, max_length, neg, dataset_name):
         self.model_name = model
+        self.epochs = epochs
         self.dataset_name = dataset_name
         self.learning_rate = learning_rate
         self.max_length = max_length
@@ -21,19 +22,39 @@ class Experiment:
         #PROCESS DATA
         self.train_dataset = self.process_dataset(neg = neg)
         self.tokenized_train_datasets = self.train_dataset.map(self.tokenize_function, batched=False)
+        #test differentiation
+        self.test_dataset_diff = self.process_dataset(dataset_path = ["data/EVAL_differentiation.json"], neg = neg, test_size = 1.0)
+        self.tokenized_test_dataset_diff = self.train_dataset.map(self.tokenize_function, batched=False)
+        self.contast_dataset_diff = self.process_dataset(dataset_path = ["data/EVAL_differentiation_VAR_SWAP.json", "data/EVAL_easy_differentiation.json"], neg = neg, test_size = 1.0)
+        self.tokenized_contrast_dataset_diff = self.train_dataset.map(self.tokenize_function, batched=False)
+        #test integration
+        self.test_dataset_int = self.process_dataset(dataset_path = ["data/EVAL_integration.json"], neg = neg, test_size = 1.0)
+        self.tokenized_test_dataset_int = self.train_dataset.map(self.tokenize_function, batched=False)
+        self.contast_dataset_int = self.process_dataset(dataset_path = ["data/EVAL_integration_VAR_SWAP.json", "data/EVAL_easy_integration.json"], neg = neg, test_size = 1.0)
+        self.tokenized_contrast_dataset_int = self.train_dataset.map(self.tokenize_function, batched=False)
         #LOAD METRICS AND MODEL
         self.metric = evaluate.load("glue", "mrpc")
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        self.eval_dict = {
+            "dev_set": self.tokenized_train_datasets["test"]
+            "test_diff" : self.tokenized_test_dataset_diff, 
+            "test_int" : self.tokenized_test_dataset_int, 
+            "contrast_diff" : self.tokenized_contrast_dataset_diff, 
+            "contrast_int" : self.tokenized_contrast_dataset_int
+            }
         self.model = TransLatentReasoning(model, 2, self.device)
 
 
     def process_dataset(self, dataset_path = ["data/differentiation.json", "data/integration.json"], neg = 1,  test_size = 0.2):
         #convert dataset into json for dataset loader
         formatted_examples = []
-        op_id = 0
         for path in dataset_path:
             d_file = open(path, 'r')
             d_json = json.load(d_file)
+            if "differentiation" in path:
+                op_id = 0
+            elif "integration" in path:
+                op_id = 1
             # create an entry for each positive example
             for example in tqdm(d_json, desc="Loading Dataset"):
                 formatted_examples.append({"equation1": example['premise_expression'], "equation2": example['variable'], "target": example["positive"], "operation": op_id, "label": 1.0})
@@ -44,7 +65,6 @@ class Experiment:
                         break
                     formatted_examples.append({"equation1": example['premise_expression'], "equation2": example['variable'], "target": negative , "operation": op_id, 'label': -1.0})
                     count_neg += 1
-            op_id += 1
         print("Data examples", formatted_examples[:4])
         #split randomly between train, dev, and test set
         dataset = Dataset.from_list(formatted_examples)
@@ -56,7 +76,7 @@ class Experiment:
     def tokenize_function(self, examples):
         examples["equation1"] = self.tokenizer(examples["equation1"], padding="max_length", truncation=True, max_length = self.max_length)
         examples["equation2"] = self.tokenizer(examples["equation2"], padding="max_length", truncation=True, max_length = self.max_length)
-        examples["target"] = self.tokenizer(examples["target"], padding="max_length", truncation=True, max_length = self.max_length) #, return_tensors='pt')
+        examples["target"] = self.tokenizer(examples["target"], padding="max_length", truncation=True, max_length = self.max_length)
         return examples
 
     def compute_metrics(self, eval_pred):
@@ -71,30 +91,15 @@ class Experiment:
     def train_and_eval(self):
         device = self.device
         self.model.to(device)
-        
         self.model.train()
         
         train_loader = DataLoader(self.tokenized_train_datasets["train"].with_format("torch"), batch_size=8, shuffle=True)
-        dev_loader = DataLoader(self.tokenized_train_datasets["test"].with_format("torch"), batch_size=4, shuffle=True)
-
-        #test_loader = DataLoader(self.tokenized_test_datasets.with_format("torch"), batch_size=4, shuffle=True)
-        #test_vr_loader = DataLoader(self.tokenized_test_vr_datasets.with_format("torch"), batch_size=4, shuffle=True)
-        #test_easy_loader = DataLoader(self.tokenized_test_easy_datasets.with_format("torch"), batch_size=4, shuffle=True)
-
-        eval_loaders = {
-                "dev":dev_loader
-                #"test":test_loader,
-                #"test_vr":test_vr_loader,
-                #"test_easy":test_easy_loader
-                }
-        
         optim = AdamW(self.model.parameters(), lr=self.learning_rate)
         
         print("Start training...")
-
         eval_steps_cycle = 2000
         steps = 0
-        for epoch in tqdm(range(32), desc = "Training"):
+        for epoch in tqdm(self.epochs, desc = "Training"):
             for batch in tqdm(train_loader):
                 steps += 1
                 optim.zero_grad()
@@ -109,70 +114,56 @@ class Experiment:
                 optim.step()
                 #evaluation
                 if steps % eval_steps_cycle == 0:
-                    self.model.eval()
-                    print("EVALUATION")
-                    num_instances = 100000
-                    for loader in eval_loaders:
-                        eval_steps = 0
-                        scores_pos = []
-                        scores_neg = []
-                        logits_metric = []
-                        logits_metric_diff = []
-                        logits_metric_int = []
-                        label_metric = []
-                        label_metric_diff = []
-                        label_metric_int = []
-                        for eval_batch in tqdm(eval_loaders[loader]):
-                            eval_steps += 1
-                            equation1 = eval_batch["equation1"]
-                            equation2 = eval_batch["equation2"]
-                            target = eval_batch["target"]
-                            labels = eval_batch["label"]
-                            operation = eval_batch['operation']
-                            outputs = self.model(equation1, equation2, target, operation, labels)
-                            batch_index = 0
-                            for score in outputs[1]:
-                                if score > 0.0:
-                                    logits_metric.append(1)
-                                    if operation[batch_index] == 0:
-                                        logits_metric_diff.append(1)
-                                    else:
-                                        logits_metric_int.append(1)
-                                else:
-                                    logits_metric.append(0)
-                                    if operation[batch_index] == 0:
-                                        logits_metric_diff.append(0)
-                                    else:
-                                        logits_metric_int.append(0)
-                                batch_index += 1
-                            batch_index = 0
-                            for label in labels:
-                                if label == 1.0:
-                                    scores_pos.append(outputs[1].detach().cpu().numpy())
-                                    label_metric.append(1)
-                                    if operation[batch_index] == 0:
-                                        label_metric_diff.append(1)
-                                    else:
-                                        label_metric_int.append(1)
-                                else:
-                                    scores_neg.append(outputs[1].detach().cpu().numpy())
-                                    label_metric.append(0)
-                                    if operation[batch_index] == 0:
-                                        label_metric_diff.append(0)
-                                    else:
-                                        label_metric_int.append(0)
-                                batch_index += 1
-                            if eval_steps > num_instances:
-                                break
-                        print("=============="+loader+"==============")
-                        print("positive:", np.mean(scores_pos))
-                        print("negative:", np.mean(scores_neg))
-                        print("difference:", np.mean(scores_pos) - np.mean(scores_neg))
-                        print("tot:", self.compute_metrics([logits_metric, label_metric]))
-                        print("differentiation:", self.compute_metrics([logits_metric_diff, label_metric_diff]))
-                        print("integration:", self.compute_metrics([logits_metric_int, label_metric_int]))
+                    self.evaluation()
                     self.model.train()
 
+
+    def evaluation(self, batch_size = 4):
+        if self.eval_dict == None:
+            print("No evaluation data found!")
+            return
+        #build dataloaders
+        eval_loaders = {}
+        for dataset_name in self.eval_dict:
+            eval_loaders[dataset_name] = DataLoader(self.eval_dict[dataset_name].with_format("torch"), batch_size=batch_size, shuffle=True)
+        #START EVALUATION
+        self.model.eval()
+        print("EVALUATION")
+        for loader in eval_loaders:
+            eval_steps = 0
+            scores_pos = []
+            scores_neg = []
+            logits_metric = []
+            label_metric = []
+            for eval_batch in tqdm(eval_loaders[loader], desc = loader):
+                eval_steps += 1
+                equation1 = eval_batch["equation1"]
+                equation2 = eval_batch["equation2"]
+                target = eval_batch["target"]
+                labels = eval_batch["label"]
+                operation = eval_batch['operation']
+                outputs = self.model(equation1, equation2, target, operation, labels)
+                batch_index = 0
+                for score in outputs[1]:
+                    if score > 0.0:
+                        logits_metric.append(1)
+                    else:
+                        logits_metric.append(0)
+                    batch_index += 1
+                batch_index = 0
+                for label in labels:
+                    if label == 1.0:
+                        scores_pos.append(outputs[1].detach().cpu().numpy())
+                        label_metric.append(1)
+                    else:
+                        scores_neg.append(outputs[1].detach().cpu().numpy())
+                        label_metric.append(0)
+                    batch_index += 1
+            print("=============="+loader+"==============")
+            print("positive avg sim:", np.mean(scores_pos))
+            print("negative avg sim:", np.mean(scores_neg))
+            print("difference:", np.mean(scores_pos) - np.mean(scores_neg))
+            print("metrics:", self.compute_metrics([logits_metric, label_metric]))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -184,7 +175,7 @@ if __name__ == '__main__':
                     help="Batch size.")
     parser.add_argument("--max_length", type=int, default=128, nargs="?",
                     help="Input Max Length.")
-    parser.add_argument("--epochs", type=float, default=12.0, nargs="?",
+    parser.add_argument("--epochs", type=int, default=32, nargs="?",
                     help="Num epochs.")
     parser.add_argument("--lr", type=float, default=3e-5, nargs="?",
                     help="Learning rate.")
@@ -210,27 +201,3 @@ if __name__ == '__main__':
             dataset_name = dataset
             )
     experiment.train_and_eval()
-
-#custom dataset
-#import torch
-
-#train_encodings = tokenizer(train_texts, truncation=True, padding=True)
-#val_encodings = tokenizer(val_texts, truncation=True, padding=True)
-#test_encodings = tokenizer(test_texts, truncation=True, padding=True)
-
-#class IMDbDataset(torch.utils.data.Dataset):
-#    def __init__(self, encodings, labels):
-#        self.encodings = encodings
-#        self.labels = labels
-
-#    def __getitem__(self, idx):
-#        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-#        item['labels'] = torch.tensor(self.labels[idx])
-#        return item
-
-#    def __len__(self):
-#        return len(self.labels)
-
-#train_dataset = IMDbDataset(train_encodings, train_labels)
-#val_dataset = IMDbDataset(val_encodings, val_labels)
-#test_dataset = IMDbDataset(test_encodings, test_labels)
