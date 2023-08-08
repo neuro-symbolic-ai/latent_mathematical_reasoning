321f16f2e807f64462899b2f881ed1cd9cbbb401
mValentino91 <marco@DESKTOP-8HJ11VO.localdomain>
2023-08-08 11:46:12 +0100

    all symbols tokenizer

--- latent_reasoning/sequential_utils.py
+++ latent_reasoning/sequential_utils.py
@@ -42,23 +42,33 @@ class Corpus(object):
                 for word in words:
                     self.dictionary.add_word(word)
 
-    def tokenizer(self, sentence:str):
-        sentence = sentence.replace("{", " { ").replace("}", " } ").replace("(", " ( ").replace(")", " ) ").replace("^", " ^ ").replace("d", " d ").replace("  ", " ")
-        words = sentence.split(" ")
-        ids = []
-        mask = []
-        word_count = 0
-        for word in words:
-            self.dictionary.add_word(word)
-            ids.append(self.dictionary.word2idx[word])
-            mask.append(0)
-            word_count += 1
-            if word_count == self.max_len:
-                break
-        for i in range(self.max_len-word_count):
-            ids.append(self.dictionary.word2idx["[PAD]"])
-            mask.append(1)
-        return {'input_ids': torch.tensor(ids).type(torch.int64), 'input_mask': torch.tensor(mask).type(torch.bool)}
+    def tokenizer(self, sentences:list, build_vocabulary = True):
+        sep_tokens = ["{", "}", "(", ")", "[", "]", "^", "d", "_", "|"]
+        tokenized_sentences = []
+        for sentence in sentences:
+            for token in sep_tokens:
+                sentence = sentence.replace(token, " " + token + " ")
+            sentence = sentence.replace("  ", " ")
+            words = sentence.split(" ")
+            ids = []
+            mask = []
+            word_count = 0
+            for word in words:
+                if build_vocabulary == True:
+                    self.dictionary.add_word(word)
+                if word in self.dictionary.word2idx:
+                    ids.append(self.dictionary.word2idx[word])
+                else:
+                    ids.append(self.dictionary.word2idx["[UNK"])
+                mask.append(0)
+                word_count += 1
+                if word_count == self.max_len:
+                    break
+            for i in range(self.max_len-word_count):
+                ids.append(self.dictionary.word2idx["[PAD]"])
+                mask.append(1)
+            tokenized_sentences.append({'input_ids': torch.tensor(ids).type(torch.int64), 'input_mask': torch.tensor(mask).type(torch.bool)})
+        return tokenized_sentences
 
     def var_tokenizer(self, sentences:list, build_vocabulary = True):
         variables = {}
@@ -73,7 +83,6 @@ class Corpus(object):
             ids = []
             mask = []
             word_count = 0
-            found_word = False
             for word in words:
                 if not "\\" in word and not word in sep_tokens and word.isalpha():
                     if not word in variables:
